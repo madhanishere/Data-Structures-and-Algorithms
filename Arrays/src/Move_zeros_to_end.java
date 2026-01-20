8faf5d40d72eba0292f38aa77782881f1c75a289
madhanraghuram <madhanraghuram@gmail.com>
2026-01-20 21:00:35 +0530

    changed

--- Arrays/src/Move_zeros_to_end.java
+++ Arrays/src/Move_zeros_to_end.java
@@ -13,6 +13,7 @@ public class Move_zeros_to_end {
         }
         for ( i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
+            System.out.println();
 
         }
     }
