fe31c505e5ea90299504f3830f4bba33ffdb80c3
madhanraghuram <madhanraghuram@gmail.com>
2026-01-23 21:57:48 +0530

    added in arrays

--- Arrays/src/Find_num_that_appear_once.java
+++ Arrays/src/Find_num_that_appear_once.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class Find_num_that_appear_once {
+    static void main(String[] args) {
+        int [] arr={1,2,2,1,3,3,4};
+        int sum=0;
+        for (int i = 0; i < arr.length; i++) {
+            sum=sum^arr[i];
+        }
+        System.out.println(sum);
+    }
 }
--- Arrays/src/Longest_Subarray_with_given_Sum_K.java
+++ Arrays/src/Longest_Subarray_with_given_Sum_K.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
-
 public class Longest_Subarray_with_given_Sum_K {
+    static void main(String[] args) {
+        int [] arr={1,2,3,1,1,1,2,3};
+        int l=0;
+
+        int sum=0;
+        int k=3;
+        int maxlen=0;
+        for (int r = 0; r < arr.length; r++){
+            sum +=arr[r];
+            while (sum>k) {
+                sum -= arr[l];
+                l++;
+            }
+                if(sum==k){
+                    maxlen=Math.max(maxlen,r-l+1);
+                }
+
+            }
+
+        System.out.println(maxlen);
+    }
 }
