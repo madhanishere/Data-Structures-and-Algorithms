adda0669d437cd92e08a455b7e4e1980c71e4f7b
madhanraghuram <madhanraghuram@gmail.com>
2026-01-26 22:22:40 +0530

    added in arrays

--- Arrays/src/Two_sum.java
+++ Arrays/src/Two_sum.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
-
 public class Two_sum {
+    static void main(String[] args) {
+        int [] arr={0,1,3,4,5,6,7};
+        int tar=7;
+        int left=0;
+        int right=arr.length-1;
+        while (left<right){
+            int sum=arr[left]+arr[right];
+            if(sum>tar){
+                right--;
+            }
+            else if(sum<tar){
+                left++;
+            } else if (sum==tar) {
+                System.out.println("true");
+                return;
+            }
+
+
+        }
+        System.out.println("false");
+    }
 }
---
+++ Arrays/src/most_frequent_element.java
@@ -0,0 +1,23 @@
+public class most_frequent_element{
+    static void main(String[] args) {
+        int []arr={7,0,7,1,7};
+        int count=0;
+        int maxcount=0;
+        int ele=Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[i]==arr[j]){
+                    count++;
+                }
+                if(count>maxcount){
+                    ele=arr[i];
+                    maxcount=count;
+                }
+            }
+            count=0;
+
+        }
+        System.out.println(maxcount);
+        System.out.println(ele);
+    }
+}
